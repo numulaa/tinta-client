3c6faab270bd2e84d8d9dd8256e9ae908a34e88a
Nurul Mukhlisa <numulaa@gmail.com>
2023-03-15 13:36:36 -1200

    updated

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import "./App.css";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import OnBoarding from "./pages/OnBoarding";
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Home/>}/>
-        <Route path={"/dashboard"} element={<Dashboard/>}/>
-        <Route path={"/onboarding"} element={<OnBoarding/>}/>
-
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/dashboard"} element={<Dashboard />} />
+        <Route path={"/onboarding"} element={<OnBoarding />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function AuthModal({ isSignUp, setShowModal }) {
+  let navigate = useNavigate();
   const [authFormData, setAuthFormData] = useState({
     userName: "",
     email: "",
@@ -9,7 +12,7 @@ export default function AuthModal({ isSignUp, setShowModal }) {
     confirmPassword: "",
   });
   function handleChange(e) {
-    const [name, value] = e.target;
+    const { name, value } = e.target;
     setAuthFormData((prevAuthFormData) => ({
       ...prevAuthFormData,
       [name]: value,
@@ -18,10 +21,24 @@ export default function AuthModal({ isSignUp, setShowModal }) {
   function closeAuth() {
     setShowModal(false);
   }
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
+
     console.log("submitted");
+
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/${isSignUp ? "signup" : "login"}`,
+        { email: authFormData.email, password: authFormData.password }
+      );
+      const success = response.status === 201;
+      if (success) navigate("/onboarding");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   }
+
   return (
     <div className="absolute left-0 right-0 ml-auto mr-auto max-w-2xl h-xl px-20 py-16 bg-white rounded-2xl ">
       <div
@@ -30,6 +47,7 @@ export default function AuthModal({ isSignUp, setShowModal }) {
       >
         X
       </div>
+      <h1 className="font-bold mb-8">CREATE ACCOUNT</h1>
       <form
         className="flex flex-col justify-between gap-2"
         onSubmit={handleSubmit}
@@ -68,6 +86,10 @@ export default function AuthModal({ isSignUp, setShowModal }) {
             className="rounded-md p-3 border-solid border-2 border-gray"
           ></input>
         )}
+        <p className="text-sm">
+          By clicking Log In / Create Account, you agree to our terms. Learn how
+          we process your data in our Privacy Policy and Cookie Policy
+        </p>
         <button className="p-3 rounded-md bg-brightOrange text-white cursor-pointer mt-4">
           {isSignUp ? "Sign In" : "Create Account"}
         </button>
